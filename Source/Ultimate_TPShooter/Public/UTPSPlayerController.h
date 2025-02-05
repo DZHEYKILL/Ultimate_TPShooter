75f85b3870680e7076f40e62c51ce2a3d89ebe38
DZHAMBOLAT KASAEV <william.ddd7@gmail.com>
2025-02-05 15:52:04 +0300

    Base

---
+++ Source/Ultimate_TPShooter/Public/UTPSPlayerController.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "UTPSPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ULTIMATE_TPSHOOTER_API AUTPSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+
+};
